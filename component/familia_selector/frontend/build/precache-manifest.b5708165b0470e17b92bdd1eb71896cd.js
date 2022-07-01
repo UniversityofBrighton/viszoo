b6f8e53a8bc53ed89ddfcdb169f3a353a39d8330
pierreHaslee <pierre.haslee@protonmail.com>
2022-07-01 16:39:45 +0100

    .gitignore modified to send the custom component build

---
+++ component/familia_selector/frontend/build/precache-manifest.b5708165b0470e17b92bdd1eb71896cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f1baccf1fbd48f5b4ce56eddb424ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "125cb09ccd0a1f3231e6",
+    "url": "./static/js/2.3b7934a9.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.3b7934a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6853d033829fe6fc7e",
+    "url": "./static/js/main.e92b83a4.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
