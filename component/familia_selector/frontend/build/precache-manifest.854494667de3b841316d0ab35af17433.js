c03ccd2531165736c5b2bc8c70ae8af46a329ffe
pierreHaslee <pierre.haslee@protonmail.com>
2022-07-04 12:29:06 +0100

    component gitignore changed

---
+++ component/familia_selector/frontend/build/precache-manifest.854494667de3b841316d0ab35af17433.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a059e505cf5ede9c312904f78b12a13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c62116d89c7fdee600df",
+    "url": "./static/js/2.56675d45.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.56675d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1eb0f315a0fd1c86358",
+    "url": "./static/js/main.aeeedc76.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
