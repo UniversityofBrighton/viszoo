ab5970b3fdd86e5fc73b9d7e5b2accebddf38752
Pierre Haslée <97137454+pierreHaslee@users.noreply.github.com>
2022-07-05 10:16:52 +0100

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def filter_data(data: pd.DataFrame, families_filter_out, type_filter_out, time1, time2):
+#there are 5 positional arguments
+def filter_data(data, families_filter_out, type_filter_out, time1, time2):
 
   filtered_data =  data.where((data['ano_coleta'] <= time2) & (data['ano_coleta'] >= time1))
   filtered_data = filtered_data.where((~filtered_data['familia'].isin(families_filter_out)))
